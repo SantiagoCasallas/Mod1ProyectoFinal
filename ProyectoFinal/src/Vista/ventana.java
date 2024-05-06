f543f5bda1e4d32254d10461d78608609db5455b
Santiago C <dscasallasr@udistrital.edu.co>
2024-05-05 19:05:39 -0500

    paquetes2

---
+++ ProyectoFinal/src/Vista/ventana.java
@@ -0,0 +1,9 @@
+package Vista;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ventana extends JFrame {
+    public JFrame frame = new JFrame();
+    public JOptionPane vista = new JOptionPane();
+}
